bb9752bf57539ab285fa6b2361e80f3d4fc6af0c
rajitha_live <rajithatcc@gmail.com>
2022-05-11 12:39:43 +0530

    SCS-103 done

--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -6,18 +6,23 @@ import { BaseService } from '../core/services/base.service';
 import { User } from '../_models/user.model';
 import { Router } from '@angular/router';
 import { CryptoService } from '../core/services/crypto.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { TokenData } from '../_models/token-data.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService  extends BaseService {
+  baseUrl = environment.baseEndpoint;
+
 
   private currentUserSource: BehaviorSubject<User|null>;
   currentUser$: Observable<User| null>;
 
   constructor(http: HttpClient,
     private cryptoService: CryptoService,
-    private router: Router) { 
+    private router: Router) {
     super(http);
     this.currentUserSource = new BehaviorSubject<User|null>(null);
     this.currentUser$ = this.currentUserSource.asObservable();
@@ -27,53 +32,65 @@ export class AccountService  extends BaseService {
     
 
   login(model: any) {
-    // return this.http.post(this.baseUrl + 'user/authenticate', model, { withCredentials: true }).pipe(
-    //   map((response: any) => {
-    //     const user = response;
-    //     if (user) {
-    //       this.setCurrentUser(user);
-    //       return user;
-    //     }
-    //   })
-    // )
+    return this.http.post(this.baseUrl + 'user/authenticate', model, { withCredentials: true }).pipe(
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          this.setCurrentUser(user);
+          return user;
+        }
+      })
+    )
   }
 
-  
+  refreshToken() {
+    return this.http.post(this.baseUrl + 'user/refresh-token', {}, { withCredentials: true }).pipe(
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          this.setCurrentUser(user);
+          return user;
+        }
+      })
+    )
+  }
+
+
+
+  logout(optionalErrorMessage? : string) {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+    this.router.navigate(['/account']);
+  }
+
+  setCurrentUser(user: User) {
+    this.startRefreshTokenTimer(user.jwtToken);
+    var enriptedUser = this.cryptoService.encrypt(JSON.stringify(user));
+    localStorage.setItem('user', enriptedUser);
+    this.currentUserSource.next(user);
+  }
+
+  removeCurrentUser() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+
+  getDecodedToken(token: string) {
+    const helper = new JwtHelperService();
+    const decodedToken = helper.decodeToken<TokenData>(token);
+    return decodedToken;
+  }
+
+ private refreshTokenTimeout: any;
+
+  private startRefreshTokenTimer(token:string) { 
+      // parse json object from base64 encoded jwt token
+      const jwtToken = JSON.parse(atob(token.split('.')[1]));
+
+      // set a timeout to refresh the token a minute before it expires
+      const expires = new Date(jwtToken.exp * 1000);
+      const timeout = expires.getTime() - Date.now() - (60 * 1000);
+      this.refreshTokenTimeout = setTimeout(() => this.refreshToken().subscribe(), timeout);
+  }
 
 }
-
-  // logout(optionalErrorMessage? : string) {
-  //   localStorage.removeItem('user');
-  //   this.currentUserSource.next(null);
-  //   this.router.navigate(['/account']);
-  // }
-
-  // setCurrentUser(user: User) {
-  //   this.startRefreshTokenTimer(user.jwtToken);
-  //   var enriptedUser = this.cryptoService.encrypt(JSON.stringify(user));
-  //   localStorage.setItem('user', enriptedUser);
-  //   this.currentUserSource.next(user);
-  // }
-
-  // removeCurrentUser() {
-  //   localStorage.removeItem('user');
-  //   this.currentUserSource.next(null);
-  // }
-
-  // getDecodedToken(token: string) {
-  //   const helper = new JwtHelperService();
-  //   const decodedToken = helper.decodeToken<TokenData>(token);
-  //   return decodedToken;
-  // }
-
- // private refreshTokenTimeout: any;
-
-  // private startRefreshTokenTimer(token:string) { 
-  //     // parse json object from base64 encoded jwt token
-  //     const jwtToken = JSON.parse(atob(token.split('.')[1]));
-
-  //     // set a timeout to refresh the token a minute before it expires
-  //     const expires = new Date(jwtToken.exp * 1000);
-  //     const timeout = expires.getTime() - Date.now() - (60 * 1000);
-  //     this.refreshTokenTimeout = setTimeout(() => this.refreshToken().subscribe(), timeout);
-  // }
--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -36,15 +36,15 @@ export class LoginComponent implements OnInit {
   login() {
     if (this.loginForm.valid) {
       this.isSubmitting = true;
-      // this.accountService.login(this.loginForm.value).subscribe(
-      //   res => {
-      //     this.router.navigate([this.returnUrl]);
-      //     console.log('Successfully logged.');
+      this.accountService.login(this.loginForm.value).subscribe(
+        res => {
+          this.router.navigate([this.returnUrl]);
+          console.log('Successfully logged.');
 
-      //   },
-      //   err => {
-      //     this.isSubmitting = false;
-      //   })
+        },
+        err => {
+          this.isSubmitting = false;
+        })
     } else {
       this.loginForm.markAllAsTouched();
     }
