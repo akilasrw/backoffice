6936757eb124d2f51970afa104090fd41e028619
rajitha_live <rajithatcc@gmail.com>
2022-05-19 11:41:09 +0530

    component route add

--- src/app/modules/booking/booking-routing.module.ts
+++ src/app/modules/booking/booking-routing.module.ts
@@ -1,7 +1,10 @@
+import { BookingSummaryComponent } from './booking-summary/booking-summary.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BookingSummaryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
