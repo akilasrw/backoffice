c2a088a947bcaae873f4807d8e4378c33710012c
rajitha_live <rajithatcc@gmail.com>
2022-06-24 15:12:52 +0530

    import changes

--- src/app/_models/view-models/Airport/airport.model.ts
+++ src/app/_models/view-models/Airport/airport.model.ts
@@ -1,11 +1,9 @@
-export class Airport{
-  id?:string;
-  name?: string;
-  code?: string;
-  countryName?: string;
-  countryId?:string;
-  lat?:number;
-  lon?: number;       
+export class Airport {
+    id?:string;
+    name?: string;
+    code?: string;
+    countryName?: string;
+    countryId?:string;
+    lat?:number;
+    lon?: number; 
 }
-
-
