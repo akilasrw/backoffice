936d9aedba4abda372e68be7f336856fb83c0304
rajitha_live <rajithatcc@gmail.com>
2022-08-01 13:02:48 +0530

    SCS-169 partly done

--- src/app/_models/view-models/booking-summary/cargo-booking-summary.model.ts
+++ src/app/_models/view-models/booking-summary/cargo-booking-summary.model.ts
@@ -1,4 +1,12 @@
+import { AircraftConfigType } from "src/app/core/enums/common-enums";
 
 export interface CargoBookingSummary {
-    
+    flightNumber:string;
+    scheduledDepartureDateTime:Date;
+    originAirportCode:string;
+    destinationAirportCode:string;
+    originAirportName:string;
+    destinationAirportName:string;
+    aircraftRegNo:string;
+    aircraftConfigurationType:AircraftConfigType;
 }
--- src/app/modules/booking/booking-summary/booking-summary-list/booking-summary-list.component.ts
+++ src/app/modules/booking/booking-summary/booking-summary-list/booking-summary-list.component.ts
@@ -1,4 +1,8 @@
+import { CargoBookingSummary } from './../../../../_models/view-models/booking-summary/cargo-booking-summary.model';
 import { Component, OnInit } from '@angular/core';
+import { BookingSummaryService } from 'src/app/_services/booking-summary.service';
+import { BookingSummaryFilterQuery } from 'src/app/_models/queries/booking-summary/booking-summary-filter-query.model';
+import { CoreExtensions } from 'src/app/core/extensions/core-extensions.model';
 
 @Component({
   selector: 'app-booking-summary-list',
@@ -10,28 +14,66 @@ export class BookingSummaryListComponent implements OnInit {
   isLoading:boolean=false;
   filterFormHasValue:boolean=false;
   flightNumber?: string;
-  flightDate?: string;
-  constructor() { }
+  flightDate?: Date;
+  totalCount: number = 0;
+  bookingSummarys:CargoBookingSummary[]=[];
+  bookingSummaryFilterQuery: BookingSummaryFilterQuery = new BookingSummaryFilterQuery();
+
+  constructor(private bookingSummaryService:BookingSummaryService) { }
 
   ngOnInit(): void {
+    this.getFilteredList();
   }
 
   onChangeFilterFrm(event: any) {
-    // if ((this.airportName !== undefined && this.airportName !== "") ||
-    //   (this.countryName !== undefined && this.countryName !== "") ||
-    //   (this.airportCode !== undefined && this.airportCode !== "")
-    // ) {
-    //   this.filterFormHasValue = true;
-    // } else {
-    //   this.filterFormHasValue = false;
-    // }
+    if ((this.flightNumber !== undefined && this.flightNumber !== "") || (this.flightDate !== null))
+    {
+      this.filterFormHasValue = true;
+    } else {
+      this.filterFormHasValue = false;
+    }
   }
 
-  getFlightList(){
+  getFilteredList(){
+    this.isLoading=true;
+    this.bookingSummaryFilterQuery.flightNumber=this.flightNumber;
+    this.bookingSummaryFilterQuery.flightDate=this.flightDate;
 
+    this.bookingSummaryService.getFilteredList(this.bookingSummaryFilterQuery).subscribe(
+      {
+        next:(res)=>{
+          this.bookingSummarys = res.data;
+          this.totalCount = res.count;
+          this.isLoading=false;
+        },
+        error:()=>{
+          this.bookingSummarys = [];
+          this.totalCount = 0;
+          this.isLoading=false;
+        }
+      }
+    )
+  }
+
+  public onPageChanged(event: any) {
+    if (this.bookingSummaryFilterQuery?.pageIndex !== event) {
+      this.bookingSummaryFilterQuery.pageIndex = event;
+      this.getFilteredList();
+    }
   }
 
   clearFilter(){
+    this.flightNumber=undefined;
+    this.flightDate=undefined;
+    this.filterFormHasValue = false;
+  }
+
+  GetAircraftConfigType(type: number) {
+    return CoreExtensions.GetAircraftConfigType(type);
+  }
+
+  onViewDetail(item:any){
 
   }
+  
 }
