64f182c440d807b07e5eb2445bf14b02530b983d
rajitha_live <rajithatcc@gmail.com>
2022-09-12 17:28:02 +0530

    SCS-40

--- src/app/_models/view-models/flight-schedules-management/flight-schedule-management.ts
+++ src/app/_models/view-models/flight-schedules-management/flight-schedule-management.ts
@@ -1,9 +1,11 @@
+import { Time } from "@angular/common";
 import { BaseVM } from "src/app/shared/models/base-vm.model";
 
 export class FlightScheduleManagement extends BaseVM{
     flightNumber?: string;
     scheduleStartDate?: Date;
     scheduleEndDate?: Date;
+    scheduledTime?:Date;
     daysOfWeek?:string;
     originAirportCode?:string;
     destinationAirportCode?:string;
