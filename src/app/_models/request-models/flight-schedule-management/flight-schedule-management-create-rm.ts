36cd9fb09a03874f0c01a2682b13300351976537
rajitha_live <rajithatcc@gmail.com>
2022-09-23 17:35:41 +0530

    SCS-40

--- src/app/_models/request-models/flight-schedule-management/flight-schedule-management-create-rm.ts
+++ src/app/_models/request-models/flight-schedule-management/flight-schedule-management-create-rm.ts
@@ -6,5 +6,4 @@ export class FlightScheduleManagementCreateRM{
     scheduleStartDate?:string;
     scheduleEndDate?:string;
     daysOfWeek?:string;
-    isFlightScheduleGenerated?:boolean;      
 }
