bbccf76a331c1e7d1e1c01e2503a1324c517d614
yohanwdev <102813313+yohanwdev@users.noreply.github.com>
2022-11-03 19:55:32 +0530

    uld info component changes

---
+++ src/app/modules/booking/booking-summary/booking-summary-details/uld-info/uld-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uld-info',
+  templateUrl: './uld-info.component.html',
+  styleUrls: ['./uld-info.component.scss']
+})
+export class UldInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
