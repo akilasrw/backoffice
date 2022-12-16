0dfc060fbfbdf63e165b1f5fee090f385f084348
yohanwdev <102813313+yohanwdev@users.noreply.github.com>
2022-12-16 19:51:31 +0530

    auth gurad changes in login

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginGuard } from '../core/guards/login.guard';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent}
+  { path: '', component: LoginComponent, canActivate: [LoginGuard]},
 ];
 
 @NgModule({
