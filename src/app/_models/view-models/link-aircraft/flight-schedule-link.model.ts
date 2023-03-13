ceeb45145e35bacbeeb48833a10b5377c38a416e
yohanwdev <102813313+yohanwdev@users.noreply.github.com>
2023-03-13 17:54:22 +0530

    View Summary changes

--- src/app/_models/view-models/link-aircraft/flight-schedule-link.model.ts
+++ src/app/_models/view-models/link-aircraft/flight-schedule-link.model.ts
@@ -5,4 +5,14 @@ export class FlightScheduleLink {
   destinationAirportCode? : string;
   scheduleStartDate?:string
   status?: number;
+  aircraftSubTypeName?: string;
+  originAirportName?: string;
+  destinationAirportName?: string;
+  aircraftRegNo?: string;
+  scheduledDepartureDateTime?: Date;
+  scheduledArrivalDateTime?: Date;
+  estimatedDepartureDateTime?: Date;
+  estimatedArrivalDateTime?: Date;
+  actualDepartureDateTime?: Date;
+  actualArrivalDateTime?: Date;
 }
