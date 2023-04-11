6b0c1b24c3729bbd9eb9c5094ca846ae28baa724
yohanwdev <102813313+yohanwdev@users.noreply.github.com>
2023-04-11 21:43:52 +0530

    stand by cargo view changes

---
+++ src/app/modules/booking/stand-by-cargo-master/offload-booking-view/offload-booking-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-offload-booking-view',
+  templateUrl: './offload-booking-view.component.html',
+  styleUrls: ['./offload-booking-view.component.scss']
+})
+export class OffloadBookingViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/booking/stand-by-cargo-master/stand-by-cargo-master.component.ts
+++ src/app/modules/booking/stand-by-cargo-master/stand-by-cargo-master.component.ts
@@ -3,6 +3,7 @@ import { ToastrService } from 'ngx-toastr';
 import { CargoBookingListQuery } from 'src/app/_models/queries/cargo-bookings/cargo-booking-list-query.model';
 import { CargoBooking } from 'src/app/_models/view-models/cargo-bookings/cargo-booking.model';
 import { BookingService } from 'src/app/_services/booking.service';
+import { StandByCargoType } from 'src/app/core/enums/common-enums';
 
 @Component({
   selector: 'app-stand-by-cargo-master',
@@ -13,19 +14,22 @@ export class StandByCargoMasterComponent implements OnInit {
 
   query: CargoBookingListQuery = new CargoBookingListQuery();
   cargoBookingList: CargoBooking[] = [];
-  selectedStandByStatus = 1;
+  selectedStandByStatus = StandByCargoType;
+  standByCargoType: StandByCargoType = StandByCargoType.Offload;
 
   constructor(private bookingService: BookingService,
     private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.getBookingList();
   }
 
   getBookingList() {
-    this.query.standByStatus = this.selectedStandByStatus;
+    this.query.standByStatus = Number(this.standByCargoType);
     this.bookingService.getBookingList(this.query).subscribe(
       {
         next: (res) => {
+          console.log('StandByCargo',res);
           this.cargoBookingList = res;
         },
         error: () => {
@@ -33,6 +37,13 @@ export class StandByCargoMasterComponent implements OnInit {
         }
       }
     )
+  }
+
+  changeMenu(type: StandByCargoType) {
+    if(this.standByCargoType != type) {
+      this.standByCargoType = type;
+      this.getBookingList();
+    }
   }
 
 }
