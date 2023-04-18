e25c83a057118de483c4dd8c839635fcfaeea920
yohanwdev <102813313+yohanwdev@users.noreply.github.com>
2023-04-18 20:27:52 +0530

    stand by cargo save changes

--- src/app/modules/booking/stand-by-cargo-update/stand-by-cargo-update.component.ts
+++ src/app/modules/booking/stand-by-cargo-update/stand-by-cargo-update.component.ts
@@ -1,12 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { Subscription } from 'rxjs';
 import { CargoBookingDetailQuery } from 'src/app/_models/queries/cargo-bookings/cargo-booking-detail-query.model';
 import { User } from 'src/app/_models/user.model';
 import { BookingService } from 'src/app/_services/booking.service';
+import { FlightService } from 'src/app/_services/flight.service';
 import { AccountService } from 'src/app/account/account.service';
 import { BookingStatus } from 'src/app/core/enums/common-enums';
 import { CoreExtensions } from 'src/app/core/extensions/core-extensions.model';
+import { SelectList } from 'src/app/shared/models/select-list.model';
 
 @Component({
   selector: 'app-stand-by-cargo-update',
@@ -15,22 +18,55 @@ import { CoreExtensions } from 'src/app/core/extensions/core-extensions.model';
 })
 export class StandByCargoUpdateComponent implements OnInit {
 
+  public flightForm!: FormGroup;
+  keyword = 'value';
   isLoading: boolean= false;
   cargoBookingDetail?: any;
-  //currentUser?: User | null;
-  //subscription?: Subscription;
+  flightList: SelectList[] = [];
   editBookingId?: string;
+  startMinDate = new Date();
+
   @Input() set cargoBookingId(cargoBookingId: string) {
-    this.editBookingId = cargoBookingId;
-    //this.getCurrentUser();
+    this.editBookingId = cargoBookingId;;
     this.getBookingDetail();
   }
+  @Output() closePopup = new EventEmitter<any>();
+  @Output() submitSuccess = new EventEmitter<any>();
 
   constructor(private bookingSerice: BookingService,
     private accountService: AccountService,
-    private toastr: ToastrService,) { }
+    private toastr: ToastrService,
+    private flightService: FlightService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+    this.loadFlights();
+  }
+
+  initializeForm() {
+    this.flightForm = new FormGroup({
+      flightId: new FormControl(null, [Validators.required]),
+      departureFlightDate: new FormControl(null, [Validators.required]),
+    });
+  }
+
+  loadFlights() {
+    this.isLoading = true;
+    this.flightService.getSelectList()
+      .subscribe(res => {
+        this.isLoading = false;
+        if (res.length > 0) {
+          this.flightList = res;
+        }
+      });
+  }
+
+  selectedFlight(value: any) {
+    this.flightForm.get('flightId')?.patchValue(value.id);
+  }
+
+  onClearFlight() {
+    this.flightForm.get('flightId')?.patchValue(null);
   }
 
   getBookingDetail() { debugger;
@@ -59,18 +95,27 @@ export class StandByCargoUpdateComponent implements OnInit {
     return BookingStatus;
   }
 
-  // getCurrentUser() {
-  //   this.subscription = this.accountService.currentUser$.subscribe(res => {
-  //     this.currentUser = res;
-  //   });
-  // }
+  closeModal(){
+    this.closePopup.emit();
+  }
 
-  // getCargoAgentDetails(userId:string){
-  //   var query = new CargoAgentQuery();
-  //   query.appUserId = userId;
-  //   query.isCountryInclude=true;
-  //   this.accountService.getUserDetail(query).subscribe(res => {
-  //     this.cargoAgent = res;
-  //   });
-  // }
+  save() {
+    if (this.flightForm.get('flightId')?.value === null || this.flightForm.get('flightId')?.value === "") {
+      this.toastr.error('Please select flight.');
+      return;
+    }
+
+    if (this.flightForm.get('departureFlightDate')?.value == null) {
+      this.toastr.error('Please select date.');
+      return;
+    }
+
+
+    if (this.flightForm.valid) {
+      //this.isLoading = true;
+
+    } else {
+      this.flightForm.markAllAsTouched();
+    }
+  }
 }
