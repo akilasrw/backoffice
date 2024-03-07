71e81fa507965b902e9656985fe1cd139285017f
Thathsara Senanayaka <thathsarasenanayaka200@gmail.com>
2024-03-07 20:00:40 +0530

    update logo

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
   baseEndpoint: 'https://aeroclub-skytechcargo-app-dev-002-qa.azurewebsites.net/api/v1/',
   encriptionKey:"123456$#@$^@1ERF",
-  mainLogoPath: "/assets/images/world-cargo-airlines.svg",
-  mainIconPath: "/assets/images/world-cargo-airline-logo.svg",
-  loginImgPath: "/assets/images/wc-login-logo-bcs.svg",
+  mainLogoPath: "/assets/images/ikigai-cargo.svg",
+  mainIconPath: "/assets/images/ikigai-logo-icon.svg",
+  loginImgPath: "/assets/images/logo.svg",
+
 };
