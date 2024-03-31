88ce6efab288b6052125c97ec3ce382f6ab8116a
Lakshika Abesingha <lakshika.ma94@gmail.com>
2024-04-01 01:55:16 +0200

    Add dashboard UI changes.

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import {BsDatepickerModule} from "ngx-bootstrap/datepicker";
+import {SharedModule} from "../../shared/shared.module";
 
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        HomeRoutingModule,
+        BsDatepickerModule,
+        SharedModule
+    ]
 })
 export class HomeModule { }
