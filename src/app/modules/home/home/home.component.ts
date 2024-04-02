068282f29914238ebcb69862e16356ae5fb2108a
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2024-04-02 21:58:59 +0530

    CHG | Add Parcels Delivered table to Dashboard UI

--- src/app/modules/home/home/home.component.ts
+++ src/app/modules/home/home/home.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  isLoading: boolean = false;
+  parcelDelivered: any[] = [];
+  fileterDate?: Date = new Date(); 
+  filterFormHasValue:boolean=false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.parcelDelivered = [
+      {
+        collectedDate: '1-April',
+        AWBs: 12,
+        parcelsCollected: 1588,
+        parcelsReturned: 27,
+        parcelsOnHold: 230,
+        ULDPacked: 12,
+        onRoute: 12,
+        parcelsDelivered: 1588,
+        '24hrs': 1588,
+        '24to36hrs': 12,
+        '36hrs': 87
+      },
+    ];
+  }
+
+  onChangeFilterFrm(event: any) {
+    if ((this.fileterDate !== null))
+    {
+      this.filterFormHasValue = true;
+    } else {
+      this.filterFormHasValue = false;
+    }
   }
 
 }
