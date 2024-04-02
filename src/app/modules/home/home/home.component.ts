b2f824d435463c8afea6f9edd4aac00f11e8b65b
Lakshika Abesingha <lakshika.ma94@gmail.com>
2024-04-02 17:46:20 +0200

    Add dashboard UI changes.

--- src/app/modules/home/home/home.component.ts
+++ src/app/modules/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  public chart: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.createChart();
+  }
+  createChart(){
+
+    this.chart = new Chart("MyChart", {
+      type: 'bar', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12'],
+        datasets: [
+          {
+            label: "Total Delivery",
+            data: ['400','576', '100'],
+            backgroundColor: '#338CE5',
+            borderRadius: 6,
+            barThickness:30
+          }
+        ]
+      },
+      options: {
+        aspectRatio:1.3
+      }
+
+    });
   }
 
 }
