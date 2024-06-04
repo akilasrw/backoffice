e62921ed1c54137ea4d4f16e7ffe25e4ad14c7f7
Thathsara Senanayaka <thathsarasenanayaka200@gmail.com>
2024-06-04 14:16:37 +0530

    enable disabled old features

--- src/app/core/components/side-nav/side-nav.component.ts
+++ src/app/core/components/side-nav/side-nav.component.ts
@@ -26,50 +26,24 @@ export class SideNavComponent implements OnInit, OnDestroy {
   @Output() publishNotification = new EventEmitter<any>();
   @Output() showMsg = new EventEmitter<any>();
 
-  // sideMenuItems=[
-  //               { title:'Manage Booking',expanded: false, icon:'ca-icn-booking',
-  //                 children:[
-  //                   {title:'Booking Summary', menuType:MenuType.BookingSummay},
-  //                   {title:'Track Booking', menuType:MenuType.TrackBooking},
-  //                   {title:'Agent Bookings', menuType:MenuType.AgentBooking},
-  //                   {title:'Standby Cargo', menuType:MenuType.StandByCargo}]},
-  //               { title:'Fleet Management',expanded: false, icon:'ca-icn-flight-schedule',
-  //                 children:[
-  //                   {title:'Flight Schedule', menuType:MenuType.FlightSchedule},
-  //                   {title:'Master Schedule', menuType:MenuType.MasterSchedule},
-  //                   {title:'Assign Aircrafts', menuType:MenuType.LinkAircraft},
-  //                   {title:'Fleet Analysis', menuType:MenuType.FleetReport}]},
-  //               { title:'Agent and Rate',expanded: false, icon:'ca-icn-dollar',
-  //                 children:[
-  //                   {title:'Cargo Agent', menuType:MenuType.UserManagement},
-  //                   {title:'Rate Management', menuType:MenuType.Rate},
-  //                   {title:'AWB Stock Management', menuType:MenuType.AWBStackManagement}]},
-  //               { title:'Settings',expanded: false, icon:'ca-icn-settings',
-  //                 children:[
-  //                   {title:'Manage Airports', menuType:MenuType.Airport},
-  //                   {title:'Manage Sectors', menuType:MenuType.Sector},
-  //                   {title:'Manage Aircrafts', menuType:MenuType.Aircraft},
-  //                   {title:'Flight Creation', menuType:MenuType.Flight},
-  //                   {title:'User Management', menuType:MenuType.ManageUser}]},
-  //               { title:'ULD Management',expanded: false, icon:'ca-icn-pallet',
-  //                 children:[
-  //                   {title:'ULD Assignement', menuType:MenuType.PalletManagement},
-  //                   {title:'ULD Master', menuType:MenuType.ULDMaster}]},
-  //               { title:'Manage Notifications',expanded: false, icon:'ca-icn-bell',
-  //                 children:[
-  //                   {title:'Publish Notifications', menuType:MenuType.Notification}]},
-  //               { title:'Messaging',expanded: false, icon:'ca-icn-chat ',
-  //                 children:[
-  //                   {title:'Messaging', menuType:MenuType.Messaging}]}
-  //             ]
   sideMenuItems=[
                 { title:'Manage Booking',expanded: false, icon:'ca-icn-booking',
                   children:[
+                    {title:'Booking Summary', menuType:MenuType.BookingSummay},
+                    {title:'Track Booking', menuType:MenuType.TrackBooking},
                     {title:'Agent Bookings', menuType:MenuType.AgentBooking},
-                    {title:'Track Booking', menuType:MenuType.TrackBooking}]},
+                    {title:'Standby Cargo', menuType:MenuType.StandByCargo}]},
+                { title:'Fleet Management',expanded: false, icon:'ca-icn-flight-schedule',
+                  children:[
+                    {title:'Flight Schedule', menuType:MenuType.FlightSchedule},
+                    {title:'Master Schedule', menuType:MenuType.MasterSchedule},
+                    {title:'Assign Aircrafts', menuType:MenuType.LinkAircraft},
+                    {title:'Fleet Analysis', menuType:MenuType.FleetReport}]},
                 { title:'Agent and Rate',expanded: false, icon:'ca-icn-dollar',
                   children:[
-                    {title:'Cargo Agent', menuType:MenuType.UserManagement}]},
+                    {title:'Cargo Agent', menuType:MenuType.UserManagement},
+                    {title:'Rate Management', menuType:MenuType.Rate},
+                    {title:'AWB Stock Management', menuType:MenuType.AWBStackManagement}]},
                 { title:'Settings',expanded: false, icon:'ca-icn-settings',
                   children:[
                     {title:'Manage Airports', menuType:MenuType.Airport},
@@ -79,8 +53,8 @@ export class SideNavComponent implements OnInit, OnDestroy {
                     {title:'User Management', menuType:MenuType.ManageUser}]},
                 { title:'ULD Management',expanded: false, icon:'ca-icn-pallet',
                   children:[
-                    {title:'ULD Master', menuType:MenuType.ULDMaster},
-                    {title:'Truck Master', menuType:MenuType.TruckMaster}]},
+                    {title:'ULD Assignement', menuType:MenuType.PalletManagement},
+                    {title:'ULD Master', menuType:MenuType.ULDMaster}]},
                 { title:'Manage Notifications',expanded: false, icon:'ca-icn-bell',
                   children:[
                     {title:'Publish Notifications', menuType:MenuType.Notification}]},
@@ -88,6 +62,7 @@ export class SideNavComponent implements OnInit, OnDestroy {
                   children:[
                     {title:'Messaging', menuType:MenuType.Messaging}]}
               ]
+  
 
 
   constructor(
