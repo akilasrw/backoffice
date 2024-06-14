74775cbc88462d968bd3c67415bba752f2bfcc4d
Thathsara Senanayaka <thathsarasenanayaka200@gmail.com>
2024-06-14 05:58:00 +0530

    update agent rates

--- src/app/shared/models/select-list.model.ts
+++ src/app/shared/models/select-list.model.ts
@@ -2,3 +2,9 @@ export class SelectList {
   id : string | undefined;
   value : string | undefined;
 }
+
+
+export class SelectItemList {
+  id : number | undefined;
+  value : string | undefined;
+}
