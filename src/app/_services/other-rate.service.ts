b3003c80688ff64f375ded6b3bd572ad41e4979d
Thathsara Senanayaka <thathsarasenanayaka200@gmail.com>
2024-06-18 05:49:37 +0530

    add view other rates

---
+++ src/app/_services/other-rate.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { BaseService } from '../core/services/base.service';
+import { HttpClient } from '@angular/common/http';
+import { AgentOtherRatesType, ChildCategorytype, SubCategorytype } from '../_models/view-models/rate/agent-rate';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OtherRateService extends BaseService {
+  private readonly endpointEntityName = 'AgentRateManagement';
+
+  constructor(http: HttpClient) {
+    super(http);
+  }
+
+  GetSubCategories(id: string) {
+    return this.get<SubCategorytype[]>(`${this.endpointEntityName}/GetOtherSubRates/${id}`);
+  }
+
+  GetChildCategories(id: string) {
+    return this.get<ChildCategorytype[]>(`${this.endpointEntityName}/GetOtherChildRates/${id}`);
+  }
+
+  GetAgentOtherRates(id: string) {
+    return this.get<AgentOtherRatesType[]>(`${this.endpointEntityName}/GetAgentOtherRates/${id}`);
+  }
+}
--- src/app/modules/rate/other-charges/other-charges.component.ts
+++ src/app/modules/rate/other-charges/other-charges.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AgentOtherRatesType, ChildCategorytype, MainRateType, SubCategorytype } from 'src/app/_models/view-models/rate/agent-rate';
+import { OtherRateService } from 'src/app/_services/other-rate.service';
 
 @Component({
   selector: 'app-other-charges',
@@ -13,15 +15,60 @@ export class OtherChargesComponent implements OnInit {
   rateDetailModalVisibleAnimate = false;
   rateUpdateModalVisible = false;
   rateUpdateModalVisibleAnimate = false;
+  agentOtherRates:AgentOtherRatesType[]  =  [] 
 
-  constructor() { }
+  selectedMainCategory:MainRateType = MainRateType.ExportFees;
+  selectedSubCategory: string | null = null;
+  selectedChildCategory: string | null = null;
+
+  mainCategories = [
+    { categoryName: 'Export Fees', categoryType: MainRateType.ExportFees, id: 'exportFees' },
+    { categoryName: 'Import Fees', categoryType: MainRateType.ImportFees, id: 'importFees' },
+    { categoryName: 'Miscellaneous Fees', categoryType: MainRateType.MiscellaneousFees, id: 'miscellaneousFees' }
+  ];
+
+  subCategories: SubCategorytype[] = [];
+
+  childCategories: ChildCategorytype[] = [];
+
+  constructor(private otherRateService: OtherRateService,) { }
 
   ngOnInit(): void {
+    this.onMainCategoryChange()
+    this.onSubCategoryChange()
   }
 
+ 
   openAddRate() {
     this.addRatemodalVisible = true;
     setTimeout(() => (this.addRateModalVisibleAnimate = true));
+  }
+
+  onMainCategoryChange(){
+    console.log(this.selectedMainCategory)
+    this.otherRateService.GetSubCategories(this.selectedMainCategory.toString()).subscribe((x)=>{
+      this.subCategories = x
+    })
+  }
+
+  onSubCategoryChange(){
+    console.log(this.selectedSubCategory)
+    if(this.selectedSubCategory){
+      this.otherRateService.GetChildCategories(this.selectedSubCategory.toString()).subscribe((x)=>{
+        this.childCategories = x
+      })
+    }
+    
+  }
+
+  onChildCategoryChange(){
+    console.log(this.selectedChildCategory)
+    if(this.selectedChildCategory){
+      this.otherRateService.GetAgentOtherRates(this.selectedChildCategory.toString()).subscribe((x)=>{
+        this.agentOtherRates = x
+      })
+    }
+    
   }
 
   closeAddRate() {
@@ -38,6 +85,11 @@ export class OtherChargesComponent implements OnInit {
   closeRateUpdate() {
     this.rateUpdateModalVisibleAnimate = false;
     setTimeout(() => (this.rateUpdateModalVisible = false), 300);
+  }
+
+  getSubCategories(mainCategory: MainRateType) {
+    console.log(this.selectedSubCategory)
+    return this.subCategories.filter(subCategory => subCategory.categoryType === mainCategory);
   }
 
 }
